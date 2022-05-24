c28dbb3e668502e7b0dc2dd4d2fd202d56e68c15
Alex Canales <alexcanales766@gmail.com>
2022-05-23 21:00:54 -0400

    Moved config files getter.

--- tests/test_helpers.py
+++
@@ -1,9 +0,0 @@
-
-
-from app.helpers import checkIfFolderExistsAndCreateIfNot, checkInternetConnection, downloadListOfImagesFromUrl, getListOfPostsThatHaveJPGorPNG
-
-def test_checkIfFolderExistsAndCreateIfNot():
-    assert checkIfFolderExistsAndCreateIfNot('app/output') == True
-    
-def test_checkInternetConnection():
-    assert checkInternetConnection() == True
